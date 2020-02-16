336bec822498c8604df5be0266c81bf4ee0ba85f
denwira <denrwijaya@gmail.com>
2020-02-16 11:15:26 +0700

    Ready for Production

---
+++ build/precache-manifest.a7808994527e0b6419acbc5cc3703970.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfdfd5fbd44204ca85f11827a00d3c1f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f8ba77745792a8eb02d2",
+    "url": "/static/css/main.69746838.chunk.css"
+  },
+  {
+    "revision": "e6dbb9ce85019c41d57b",
+    "url": "/static/js/2.f6a29440.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.f6a29440.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8ba77745792a8eb02d2",
+    "url": "/static/js/main.941d5824.chunk.js"
+  },
+  {
+    "revision": "308753663f14c83a31a6",
+    "url": "/static/js/runtime-main.127b28b5.js"
+  },
+  {
+    "revision": "4374c8d42c80c09db309d1303a9ba0a5",
+    "url": "/static/media/pokemon-logo.4374c8d4.png"
+  }
+]);
